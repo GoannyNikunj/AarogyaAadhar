d959fe004882489b5b7381ac0b03f9759508926b
Sakshi Helwade <hsakshi28@gmail.com>
2024-07-22 18:31:25 +0530

    changes

--- src/Component/RaiseFund.jsx
+++ src/Component/RaiseFund.jsx
@@ -5,11 +5,11 @@ const RaiseFund = () => {
   return (
     <>
    <div className='container'>
-   <div className='heading text-center mb-5'>
+   <div className=' text-center mb-5 text-[30px]'>
     Raise Funds For Medical Treatment On ImpactGuru
     <hr className="divider " style={{marginTop:20,marginBottom:30}}/>
     </div>
-    <div className='sub-text text-center -mb-8'>
+    <div className='sub-text text-center text-[17px] '>
     Get emergency financial help to pay hospital bills and medication bills with online fundraising:
     </div>
    </div>
